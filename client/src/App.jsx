5322bfb15493cb0157f10ab5d44cdadd947a1280
amirticha <allagui_fawzi@outlook.fr>
2023-06-18 22:50:26 +0100

    react-app commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 
 import React, { useEffect, useState } from 'react';
 
-const API_BASE = "http://localhost:3001";
+// const API_BASE = "http://localhost:3001";
+const API_BASE = process.env.REACT_APP_API_BASE;
 
 function App() {
 
